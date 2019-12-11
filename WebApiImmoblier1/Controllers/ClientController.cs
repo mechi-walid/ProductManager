ea695af64cb41f49d348e55e897e43b1f47e3dd4
Walid.Mechi <Walid.Mechi@TUNLT007>
2019-12-11 11:45:52 +0100

    Ajoutez des fichiers projet.

---
+++ WebApiImmoblier1/Controllers/ClientController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ImmoblierManager.Models.Entities;
+using ImmoblierManager.Services.ClientServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiImmoblier1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : Controller
+    {
+
+        private readonly IClientServices _clientServices;
+
+        public ClientController(IClientServices clientServices)
+        {
+            _clientServices = clientServices;
+        }
+
+        [HttpGet]
+        [Route("{idClient}", Name = "GetClient")]
+
+        public async Task<IActionResult> GetClientByIdAsyn(Guid idClient)
+        {
+            var res = await _clientServices.GetClientByIdAsync(idClient);
+            if (res == null)
+            {
+                return NotFound();
+            }
+            return Ok(res);
+        }
+        [HttpGet]
+        [Route("/Client/GetAllClient/")]
+        public async Task<IActionResult> GetAllClients()
+        {
+            var res = await _clientServices.GetAllClientAsync();
+            if (res == null)
+            {
+                return NotFound();
+            }
+            return Ok(new { data = res });
+        }
+
+        [HttpGet]
+        [Route("{IdClient}", Name = "DeleteClient")]
+        public async Task<IActionResult> DeleteClientById([FromBody]Guid idClient)
+        {
+            var res = await _clientServices.GetClientByIdAsync(idClient);
+
+            if (res == null)
+            {
+                return NotFound($"Il n'existe pas un client avec l'id {idClient}");
+            }
+            _clientServices.DeleteClient(res);
+            if (await _clientServices.SaveChangeAsync())
+            {
+                return Ok("Client supprimé avec succès");
+            }
+            return BadRequest("Suppression échoué...");
+        }
+        [HttpGet]
+        [Route("{TypeClient}", Name = "GetClient")]
+        public IActionResult GetClientByType(TypeClient typeClient)
+        {
+            var res = _clientServices.GetClientByType(typeClient);
+
+            if (res == null)
+            {
+                return NotFound($"Il n'existe pas un client avec de type {typeClient}");
+            }
+            return Ok(new { data = res });
+        }
+        [HttpGet]
+        [Route("{IdClient}", Name = "UpdateClient")]
+        public async Task<IActionResult> UpdateClientById([FromBody]Client client, Guid idClient)
+        {
+            var res = await _clientServices.GetClientByIdAsync(idClient);
+
+            if (res == null)
+            {
+                return NotFound($"Il n'existe pas un client avec l'id {idClient}");
+            }
+            _clientServices.UpdateClient(client, idClient);
+            if (await _clientServices.SaveChangeAsync())
+            {
+                return Ok("Client modifié avec succès");
+            }
+            return BadRequest("Modification échoué...");
+        }
+
+
+    }
+}
---
+++ WebApiImmoblier1/Controllers/HomeController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ImmoblierManager.Models.Entities;
+using ImmoblierManager.Services.AgentsServices;
+using ImmoblierManager.Services.BienImmobilierservices;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApiImmoblier1.Controllers
+{
+    public class AgentController : Controller
+    {
+        private readonly IAgentServices _agentServices;
+        private readonly IBienImmobilierServices _benImmobilierServices;
+        public AgentController(IAgentServices agentServices, IBienImmobilierServices benImmobilierServices)
+        {
+            _agentServices = agentServices;
+            _benImmobilierServices = benImmobilierServices;
+        }
+
+        [HttpGet]
+        [Route("{idAgent}", Name = "GetAgent")]
+
+        public async Task<IActionResult> GetAgentByIdAsyn(Guid idAgent)
+        {
+            var res = await _agentServices.GetAgentByIdAsync(idAgent);
+            if (res == null)
+            {
+                return NotFound();
+            }
+            return Ok(res);
+        }
+        [HttpGet]
+        [Route("/Agent/GetAllAgent/")]
+        public async Task<IActionResult> GetAllAgents()
+        {
+            var res = await _agentServices.GetAllAgentAsync();
+            if (res == null)
+            {
+                return NotFound();
+            }
+            return Ok(new { data = res });
+        }
+
+        [HttpGet]
+        [Route("{IdAgent}", Name = "DeleteAgent")]
+        public async Task<IActionResult> DeleteAgentById([FromBody]Guid idAgent)
+        {
+            var res = await _agentServices.GetAgentByIdAsync(idAgent);
+
+            if (res == null)
+            {
+                return NotFound($"Il n'existe pas un agent avec l'id {idAgent}");
+            }
+            _agentServices.DeleteAgent(res);
+            if (await _agentServices.SaveChangeAsync())
+            {
+                return Ok("Agent supprimé avec succès");
+            }
+            return BadRequest("Suppression échoué...");
+        }
+        [HttpGet]
+        [Route("{idBienImmoblier}", Name = "GetAgentByIdBienImmoblier")]
+        public async Task<IActionResult> GetAgentByBienImmoblier(Guid idBienImmoblier)
+        {
+            var bienImmob = await _benImmobilierServices.GetBienImmoblierByIdAsync(idBienImmoblier);
+            if (bienImmob == null)
+            {
+                return NotFound("Bien immoblier inexistant !");
+            }
+            var res = _agentServices.GetAllAgentByAgentByBienImmoblier(bienImmob);
+
+            if (res == null)
+            {
+                return NotFound($"Il n'existe pas un agent pour cet bien-immoblier ");
+            }
+            return Ok(new { data = res });
+        }
+        [HttpGet]
+        [Route("{IdAgent}", Name = "UpdateAgent")]
+        public async Task<IActionResult> UpdateAgentById([FromBody]Agent agent, Guid idAgent)
+        {
+            var res = await _agentServices.GetAgentByIdAsync(idAgent);
+
+            if (res == null)
+            {
+                return NotFound($"Il n'existe pas un agent avec l'id {idAgent}");
+            }
+            _agentServices.UpdateAgent(agent, idAgent);
+            if (await _agentServices.SaveChangeAsync())
+            {
+                return Ok("Agent modifié avec succès");
+            }
+            return BadRequest("Modification échoué...");
+        }
+
+
+    }
+}
+
